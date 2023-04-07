cbadda896c7fa15a4933cc457e83a7f2584ed20a
Raudenbush <mark.raudenbush@capgemini.com>
2023-04-07 17:45:15 -0600

    using grid

--- first-tkinter-app/app6.py
+++ first-tkinter-app/app6.py
@@ -6,24 +6,29 @@ def greet():
     print(f"Hello, {userName.get() or 'World'}!")
 
 root = tk.Tk()
+root.title("Greeter")
+
+root.columnconfigure(0, weight=1)
 
 userName = tk.StringVar()
 
 input_frame = ttk.Frame(root, padding=(20, 10, 20,0))
-input_frame.pack(fill="both")
+input_frame.grid(row=0, column=0,sticky="EW")
 
 nameLabel = ttk.Label(input_frame, text="Name: ")
-nameLabel.pack(side="left", padx=(0, 10))
+nameLabel.grid(row=0, column=0, padx=(0, 10))
 nameEntry = ttk.Entry(input_frame, width=15, textvariable=userName)
-nameEntry.pack(side="left")
+nameEntry.grid(row=0, column=1,)
 nameEntry.focus()
 
 buttons = ttk.Frame(root, padding=(20,10))
-buttons.pack(fill="both")
+buttons.grid(row=1, column=0, sticky="EW")
+buttons.columnconfigure(0, weight=1)
+buttons.columnconfigure(1, weight=1)
 
 greetButton = ttk.Button(buttons, text="Greet", command=greet)
-greetButton.pack(side="left", fill="x", expand=True)
+greetButton.grid(row=0, column=0,sticky="EW")
 quitButton = ttk.Button(buttons, text="Quit", command=root.destroy)
-quitButton.pack(side="right", fill="x", expand=True)
+quitButton.grid(row=0, column=1,sticky="EW")
 
 root.mainloop()
